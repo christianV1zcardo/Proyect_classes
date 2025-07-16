2e05cb51e4234b235ee9739421387c5bd99b96ea
Christian Vizcardo <christianvizcardod@gmail.com>
2025-07-16 11:01:55 -0500

    mejoras tests y validación de entrada clases

--- clases.py
+++ clases.py
@@ -1,5 +1,36 @@
 class Venta:
     def __init__(self, id_venta, producto, cantidad, precio_unitario, fecha):
+        
+        if not isinstance(id_venta, int):
+            raise TypeError("El ID debe ser un int")
+        if id_venta <= 0:
+            raise ValueError("El ID debe ser mayor que 0")
+        
+        
+        if not isinstance(producto, str):
+            raise TypeError("El producto debe ser un str")
+        if not producto.strip():
+            raise ValueError("El producto no puede ser un str vacío")
+        
+        
+        if not isinstance(cantidad, (int, float)):
+            raise TypeError("La cantidad debe ser int o float")
+        if cantidad <= 0:
+            raise ValueError("La cantidad debe ser mayor que 0")
+        
+        
+        if not isinstance(precio_unitario, (int, float)):
+            raise TypeError("El precio_unitario deber ser un int o float")
+        if precio_unitario <= 0:
+            raise ValueError("El precio_unitario debe ser mayor que 0")
+        
+        
+        if not isinstance(fecha, str):
+            raise TypeError("La fecha debe ser un str")
+        if not fecha.strip():
+            raise ValueError("La fecha no puede ser un str vacío")
+        
+        
         self.id_venta = id_venta
         self.producto = producto
         self.cantidad = cantidad
@@ -11,7 +42,7 @@ class Venta:
         return total
     
     def __repr__(self):
-        string = (f"Venta(IDcaca={self.id_venta}, " 
+        string = (f"Venta(ID={self.id_venta}, " 
                   f"Producto='{self.producto}', "
                   f"Cantidad={self.cantidad}, "
                   f"PrecioUnitario={self.precio_unitario:.2f}, "
--- tests.py
+++ tests.py
@@ -12,4 +12,42 @@ def test_calcular_total_venta_basico():
     total_calculado = venta.calcular_total_venta()
     assert total_calculado == pytest.approx(12.5)
     
-    # escribir el resto de tewst
+# def test_calcular_total_venta_cantidad_0():
+#     """
+#     Verifica el total cuando la cantidad es cero
+#     """
+    
+#     venta = clases.Venta(id_venta=1, producto="Manzana", cantidad=0, 
+#                          precio_unitario=2.50, fecha="2024-07-15")
+#     total_calculado = venta.calcular_total_venta()
+#     assert total_calculado == pytest.approx(0)
+    
+# def test_calcular_total_venta_precio_0():
+#     """
+#     Verifica el total cuando el precio_unitario es cero
+#     """
+    
+#     venta = clases.Venta(id_venta=1, producto="Manzana", cantidad=5, 
+#                          precio_unitario=0, fecha="2024-07-15")
+    
+#     total_calculado = venta.calcular_total_venta()
+    
+#     assert total_calculado == pytest.approx(0)
+
+def test_calcular_total_venta_cantidad_0():
+    """
+    Verifica que cantidad 0 bote un ValueError
+    """
+    
+    with pytest.raises(ValueError):
+        clases.Venta(id_venta=1, producto="Manzana", cantidad=0, 
+                         precio_unitario=2.50, fecha="2024-07-15")
+        
+def test_calcular_total_venta_precio_0():
+    """
+    Verifica que precio 0 bote un ValueError
+    """
+
+    with pytest.raises(ValueError):
+        clases.Venta(id_venta=1, producto="Manzana", cantidad=4, 
+                         precio_unitario=0, fecha="2024-07-15")
